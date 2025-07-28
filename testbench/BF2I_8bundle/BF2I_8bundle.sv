c976653fbcc272a9e55a83f9c3233484653cb463
dori991 <jungjw0813@naver.com>
2025-07-28 10:43:07 +0900

    BF2I_8bundle

--- testbench/BF2I_8bundle/BF2I_8bundle.sv
+++ testbench/BF2I_8bundle/BF2I_8bundle.sv
@@ -14,24 +14,30 @@ module BF2I_8bundle #(
     output logic signed [WIDTH:0] dout_Q[DEPTH-1:0]
 );
 
+    logic signed [WIDTH:0] reg_dout_R[DEPTH-1:0];
+    logic signed [WIDTH:0] reg_dout_Q[DEPTH-1:0];
+
     integer i;
 
     always @(posedge clk or negedge rst_n) begin
         if (~rst_n) begin
             for (i = 0; i < DEPTH; i = i + 1) begin
-                dout_R[i] <= 0;
-                dout_Q[i] <= 0;
+                reg_dout_R[i] <= 0;
+                reg_dout_Q[i] <= 0;
             end
         end else begin
             if (en) begin
                 for (i = 0; i < OFFSET; i = i + 1) begin
-                    dout_R[i]        <= din_R[i] + din_R[i+OFFSET];
-                    dout_R[i+OFFSET] <= din_R[i] - din_R[i+OFFSET];
-                    dout_Q[i]        <= din_Q[i] + din_Q[i+OFFSET];
-                    dout_Q[i+OFFSET] <= din_Q[i] - din_Q[i+OFFSET];
+                    reg_dout_R[i]        <= din_R[i] + din_R[i+OFFSET];
+                    reg_dout_R[i+OFFSET] <= din_R[i] - din_R[i+OFFSET];
+                    reg_dout_Q[i]        <= din_Q[i] + din_Q[i+OFFSET];
+                    reg_dout_Q[i+OFFSET] <= din_Q[i] - din_Q[i+OFFSET];
                 end
             end
         end
     end
+
+    assign dout_R = reg_dout_R;
+    assign dout_Q = reg_dout_Q;
 
 endmodule
